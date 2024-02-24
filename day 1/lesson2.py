2b65f3c3295037d93f81bf68cb63a7afb26b782b
Nikaturiashvili <nikaturiashvili5@gmail.com>
2024-02-24 11:57:45 +0400

    first commit

---
+++ day 1/lesson2.py
@@ -0,0 +1,42 @@
+name = "nika"
+#name არის ცვლადი
+# = არის  ცვლადისტვის მნიშვნელობის მამნიჭებელი სიმბოლო
+# "nika" არის ცვლადისთვის მნიშვნელობა
+
+surname = "turiashvili"
+
+# print(name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "nika"  #ეს არის str (string) ტიპის ცვლადი
+age = 11 # ეს არის Int  ( intereger )  მთელი რიცხვი
+height = 150.4 #ეს არის float  ტიპის ცვლადი ( ათწილიადი )
+#booLean ( booL ) ტიპის ცვლადი
+
+knows_programming = False #True ან False
+is_ugly = False
+
+isUgly = False # ჯავასკრიპტული cameLcase
+
+
+#print(name + " " + surname)
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+#print (name + age)
+
+ # print(type(age))
+ # print(type(name))
+ # print(type(surname))
+ # print(type(height))
+ # print(type(knows_programming)) 
+
+print("Hi my name is")
+print(str(name))
+print("I am")
+print(str(age))
+print("My surname is")
+print(str(surname))
+print("My height is")
+print(str(height))
+print("i know proggramming")
+print(str(knows_programming))
