5c409f74706aa2e5230f32d216ee60b168164b5e
Nikaturiashvili <nikaturiashvili5@gmail.com>
2024-06-05 00:00:03 +0400

    commit

---
+++ day 25/homework1.py
@@ -0,0 +1,15 @@
+Chads = ["Davit Grdzelishvili", "Nika Keshelava", "Davit janezashvili"]
+(len(Chads))
+
+
+movies = ["Breaking Bad", "Jumanji"]
+movies.append("Titanic")
+print(movies)
+
+subjects = ["English", "Math", "Russian"]
+subjects.insert(1, "German")
+print(subjects)
+
+movies = ["Avatar","Titanic","Jumanji"]
+movies.pop(2)
+print(movies)
---
+++ day 25/homework2.py
@@ -0,0 +1,6 @@
+List = ["Eggs", "Butter", "Oil", "Cheese"]
+List.append("Meat")
+List.pop(1)
+List.insert(2, "Dog food")
+(len(List))
+print(List)
