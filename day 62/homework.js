51234d2d371494ccdd7674e6abd5d9c71d6208a5
Nikaturiashvili <nikaturiashvili5@gmail.com>
2024-10-08 21:23:25 +0400

    commit

---
+++ day 62/homework.js
@@ -0,0 +1,10 @@
+let name = ('Nika')
+
+let surname = ('Turiashvili')
+
+name = ('Datuna')
+
+surname = ('Shubashvili')
+
+console.log(name)
+console.log(surname)
---
+++ hackathon v7/hard2.py
@@ -0,0 +1,10 @@
+"ფუნქციას გადაეცემა სია. დააბრუნეთ True, თუ ამ სიაში მეორდება ელემენტი ორჯერ. სხვა შემთხვევაში დააბრუნეთ False. f([4,6,3,7,4,9,8]) = True. ახსნა: 4 მეორდება 2-ჯერ."
+
+element = ([4,6,3,7,4,9,8])
+repeated = True
+unrepeated = False
+
+if element is repeated:
+    print(True)
+else:
+    print(False)
