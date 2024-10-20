84dde2d2f36ca4510e78eba43cbec6db85f6576a
Nikaturiashvili <nikaturiashvili5@gmail.com>
2024-10-20 20:18:16 +0400

    commit

---
+++ day 63/homework.py
@@ -0,0 +1,25 @@
+#გააკეთეთ მაგალითი ყველა მათემათიკურ ოპერატორზე, +,-,*,/
+
+#friend_age = 13
+#my_age = 11
+
+#print(friend_age-my_age)
+#my friend is 2 years older than me
+
+#school_things_price = 30
+#grocceries_price = 40
+#whats the total
+#print(school_things_price+grocceries_price)
+#total is 70$
+
+
+#water_price = 0.75
+#bought = 2
+
+#print(water_price * 2)
+
+
+apple = 10
+#divided by 2 to give half to his friend
+
+print(apple / 2)
---
+++ day 72/classwork2.js
@@ -0,0 +1,5 @@
+function makeNegative(num) {
+   makeNegative(10); return -10
+    makeNegative(20); return -20
+    makeNegative(0); return 0
+}
